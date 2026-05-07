03eb9e5f7f17817e7112aedd43a299611ab1f505
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2026-05-07 18:41:06 +0700

    chore: remove temporary test files

--- apps/web/test-list.ts
+++
@@ -1,8 +0,0 @@
-import { createORPCClient } from '@orpc/client'
-import { RPCLink } from '@orpc/client/fetch'
-
-const link = new RPCLink({ url: () => new URL('http://127.0.0.1:27777/rpc') })
-const client = createORPCClient(link)
-
-const list = await client.jobs.list()
-console.log('list:', JSON.stringify(list, null, 2))
--- apps/web/test-long.ts
+++
@@ -1,8 +0,0 @@
-import { createORPCClient } from '@orpc/client'
-import { RPCLink } from '@orpc/client/fetch'
-
-const link = new RPCLink({ url: () => new URL('http://127.0.0.1:27777/rpc') })
-const client = createORPCClient(link)
-
-const result = await client.jobs.start({ prompt: 'write a 50 word story about a cat', cwd: '/tmp' })
-console.log('jobId:', result.jobId)
