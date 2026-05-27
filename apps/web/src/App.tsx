9deb30541ee0f99a319d7493ae1236d2a38acb06
fdarian <62016900+fdarian@users.noreply.github.com>
2026-05-27 23:48:54 +0000

    style: apply formatting

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { AliasesPage } from './pages/AliasesPage.tsx';
 import { ConsolePage } from './pages/ConsolePage.tsx';
 
