dc26ed38ac6381bd14d10df829eefc2ca60db311
fdarian <62016900+fdarian@users.noreply.github.com>
2026-09-26 02:42:25 +0000

    style: apply formatting

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -1,7 +1,7 @@
-import { Button as ButtonPrimitive } from '@base-ui/react/button';
+import type { Button as ButtonPrimitive } from '@base-ui/react/button';
 import { useRender } from '@base-ui/react/use-render';
 import { cva, type VariantProps } from 'class-variance-authority';
-import * as React from 'react';
+import type * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
